ad18d3cf4fbb7cf887b02661d2d8c95d79d348f5
Mohd Aiman Jaffer <aiman.jaffer@gmail.com>
2020-07-19 16:54:56 +0530

    Day 8 - 3 Sum

---
+++ ThreeSum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class ThreeSum{
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0;i<nums.length-2;i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                int low = i+1;
+                int high = nums.length-1;
+                int sum = 0 - nums[i];
+                while(low<high){
+                    if(nums[low]+nums[high] == sum){
+                        result.add(Arrays.asList(nums[low],nums[high],nums[i]));
+                        while(low<high &&nums[low]==nums[low+1])low++;
+                        while(low<high &&nums[high]==nums[high-1])high--;
+                        low++;
+                        high--;
+                    }else if(nums[low]+nums[high]<sum)
+                        low++;
+                     else if(nums[low]+nums[high]>sum)
+                        high--;
+                }
+            }
+        }
+        return result;
+    }
+}
