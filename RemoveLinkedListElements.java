b740af039845f674fce8b589e2fe4c58f5b993a7
Mohd Aiman Jaffer <aiman.jaffer@gmail.com>
2020-07-22 09:59:10 +0530

    RemoveLinkedListElements.java uncommented ListNode class

--- RemoveLinkedListElements.java
+++ RemoveLinkedListElements.java
@@ -1,13 +1,11 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ 
 class RemoveLinkedListElements{
     public ListNode removeElements(ListNode head, int val) {
         while(head !=null && head.val == val)
