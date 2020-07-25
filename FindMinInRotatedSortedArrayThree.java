8b4765677d8244a4ee0c33efd11149429fbd6985
Mohd Aiman Jaffer <aiman.jaffer@gmail.com>
2020-07-25 13:20:32 +0530

    Added a comment

--- FindMinInRotatedSortedArrayThree.java
+++ FindMinInRotatedSortedArrayThree.java
@@ -1,4 +1,5 @@
 class FindMinInRotatedSortedArrayThree{
+    //Not ideal solution but runs in O(n)
     public int findMin(int[] nums) {
         int smallest = nums[0];
         for(int i=1;i<nums.length;i++){
